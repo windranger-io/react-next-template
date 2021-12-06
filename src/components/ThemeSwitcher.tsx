288f20804b14877c0dffb806b906fd687ace1e60
jasheal <7109973+jasheal@users.noreply.github.com>
2021-12-06 14:20:22 +0000

    feat: darkmode hook (#2)

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,28 +1,8 @@
-import { useEffect, useState } from 'react'
 import { MoonIcon, LightBulbIcon } from '@heroicons/react/outline'
+import useDarkMode from 'hooks/useDarkMode'
 
 export const Switcher = () => {
-  const [darkMode, setDarkMode] = useState(false)
-
-  const toggle = () => {
-    setDarkMode(!darkMode)
-
-    if (JSON.stringify(!darkMode) === 'true') {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-
-    localStorage.setItem('darkMode', JSON.stringify(!darkMode))
-  }
-
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem('darkMode')
-
-    if (isDarkMode && isDarkMode === 'true') {
-      setDarkMode(true)
-    }
-  }, [])
+  const { darkMode, toggle } = useDarkMode()
 
   return (
     <button
