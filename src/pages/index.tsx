b371412b6d18f2caf28519b764ce6cf945039560
Jason Healy <7109973+jasonhealy@users.noreply.github.com>
2021-12-22 11:51:24 +0000

    refactor: make default view blank for faster initial deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,8 @@
-import Stacked from 'layouts/Stacked'
-import Page from 'layouts/Page'
-import Card from 'components/Card'
-import DynamicTable from 'components/DynamicTable'
-
 const Home = () => {
   return (
-    <Stacked>
-      <Page title="Dashboard Components" fullWidth>
-        <div className="px-4 py-8 sm:px-0">
-          <div className="flex flex-col">
-            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <Card>
-                  <DynamicTable />
-                </Card>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Page>
-    </Stacked>
+    <div className="flex flex-1 h-full items-center justify-center">
+      <p>Something is happening...</p>
+    </div>
   )
 }
 
