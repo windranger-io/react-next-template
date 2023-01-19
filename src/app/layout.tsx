d27dfc3d3b5633f94374cff251df20107a36d9cb
jasheal <7109973+jasonhealy@users.noreply.github.com>
2023-01-19 09:58:08 +0000

    refactor: upgrade template to next13

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/global.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
--- src/components/styles.ts
+++
@@ -1,3 +0,0 @@
-import tw from 'twin.macro'
-
-export const FlexContainer = tw.div`flex h-32 bg-gray-100 justify-center items-center`
